5c59b3288bfa230c07598325f359fa9318d5a05a
Leo Andersoon <79702313+leoandersoon@users.noreply.github.com>
2021-05-31 19:14:20 +0300

    Add files via upload

--- pingpong.py
+++ pingpong.py
@@ -1,26 +1,26 @@
 import turtle
 
-pencere = turtle.Screen()
-pencere.title("PinPong")
-pencere.bgcolor("black")
-pencere.setup(width=800, height=600)
-pencere.tracer(0)
+window = turtle.Screen()
+window.title("PinPong")
+window.bgcolor("black")
+window.setup(width=800, height=600)
+window.tracer(0)
 
-raket_a = turtle.Turtle()
-raket_a.speed(0)
-raket_a.shape('square')
-raket_a.color('white')
-raket_a.penup()
-raket_a.goto(-350,0)
-raket_a.shapesize(5, 1)
+racket_a = turtle.Turtle()
+racket_a.speed(0)
+racket_a.shape('square')
+racket_a.color('white')
+racket_a.penup()
+racket_a.goto(-350,0)
+racket_a.shapesize(5, 1)
 
-raket_b = turtle.Turtle()
-raket_b.speed(0)
-raket_b.shape('square')
-raket_b.color('white')
-raket_b.penup()
-raket_b.goto(350,0)
-raket_b.shapesize(5, 1)
+racket_b = turtle.Turtle()
+racket_b.speed(0)
+racket_b.shape('square')
+racket_b.color('white')
+racket_b.penup()
+racket_b.goto(350,0)
+racket_b.shapesize(5, 1)
 
 
 ball = turtle.Turtle()
@@ -31,47 +31,47 @@ ball.penup()
 ball.dx = 0.15
 ball.dy = 0.15
 
-yazi = turtle.Turtle()
-yazi.speed(0)
-yazi.color('white')
-yazi.penup()
-yazi.goto(0,260)
-yazi.write("Player A: 0     Player B: 0", align='center', font=('Courier', 24, 'bold'))
-yazi.hideturtle()
-puan_a = 0
-puan_b = 0
+score_board = turtle.Turtle()
+score_board.speed(0)
+score_board.color('white')
+score_board.penup()
+score_board.goto(0,260)
+score_board.write("Player A: 0     Player B: 0", align='center', font=('Courier', 24, 'bold'))
+score_board.hideturtle()
+score_a = 0
+score_b = 0
 
-def raket_a_up():
-    y = raket_a.ycor()
+def racket_a_up():
+    y = racket_a.ycor()
     y = y + 20
-    raket_a.sety(y)
+    racket_a.sety(y)
 
-def raket_a_down():
-    y = raket_a.ycor()
+def racket_a_down():
+    y = racket_a.ycor()
     y = y - 20
-    raket_a.sety(y)
+    racket_a.sety(y)
 
-def raket_b_up():
-    y = raket_b.ycor()
+def racket_b_up():
+    y = racket_b.ycor()
     y = y + 20
-    raket_b.sety(y)
+    racket_b.sety(y)
 
-def raket_b_down():
-    y = raket_b.ycor()
+def racket_b_down():
+    y = racket_b.ycor()
     y = y - 20
-    raket_b.sety(y)
+    racket_b.sety(y)
 
-pencere.listen()
-pencere.onkeypress(raket_a_up,'w')
-pencere.onkeypress(raket_a_down,'s')
-pencere.onkeypress(raket_b_up,'Up')
-pencere.onkeypress(raket_b_down,'Down')
+window.listen()
+window.onkeypress(racket_a_up,'w')
+window.onkeypress(racket_a_down,'s')
+window.onkeypress(racket_b_up,'Up')
+window.onkeypress(racket_b_down,'Down')
 
 
 
 
 while True:
-    pencere.update()
+    window.update()
     ball.setx(ball.xcor() + ball.dx)
     ball.sety(ball.ycor() + ball.dy)
 
@@ -82,21 +82,21 @@ while True:
     if ball.xcor()>390:
         ball.goto(0, 0)
         ball.dx = ball.dx * -1
-        puan_a = puan_a + 1
-        yazi.clear()
-        yazi.write("Player A: {}     Player B: {}".format(puan_a, puan_b), align='center', font=('Courier', 24, 'bold'))
+        score_a = score_a + 1
+        score_board.clear()
+        score_board.write("Player A: {}     Player B: {}".format(score_a, score_b), align='center', font=('Courier', 24, 'bold'))
 
     if ball.xcor()<-390:
         ball.goto(0, 0)
         ball.dx = ball.dx * -1
-        puan_b = puan_b + 1
-        yazi.clear()
-        yazi.write("Player A: {}     Player B: {}".format(puan_a, puan_b), align='center', font=('Courier', 24, 'bold'))
+        score_b = score_b + 1
+        score_board.clear()
+        score_board.write("Player A: {}     Player B: {}".format(score_a, score_b), align='center', font=('Courier', 24, 'bold'))
 
-    if (ball.xcor()>340 and ball.xcor()<350) and (ball.ycor()<raket_b.ycor()+60 and ball.ycor()>raket_b.ycor()-60):
+    if (ball.xcor()>340 and ball.xcor()<350) and (ball.ycor()<racket_b.ycor()+60 and ball.ycor()>racket_b.ycor()-60):
         ball.setx(340)
         ball.dx = ball.dx * -1
 
-    if (ball.xcor()<-340 and ball.xcor()>-350) and (ball.ycor()<raket_a.ycor()+60 and ball.ycor()>raket_a.ycor()-60):
+    if (ball.xcor()<-340 and ball.xcor()>-350) and (ball.ycor()<racket_a.ycor()+60 and ball.ycor()>racket_a.ycor()-60):
         ball.setx(-340)
         ball.dx = ball.dx * -1
